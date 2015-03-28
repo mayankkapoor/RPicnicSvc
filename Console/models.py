f4e2bd8c9459a0c9ddb823878c52fee0b36d9f46
Shashank Chakelam <shashankchakelam@gmail.com>
2015-03-28 18:33:32 +0530

    Created basic model, added Consoel to INSTALLED_APPS and changed database to point to RDS

--- Console/models.py
+++ Console/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    role = models.CharField(max_length=20)
+    phone_number = models.IntegerField()
+
+class Depot(models.Model):
+    depot_number = models.IntegerField();
+    depot_address = models.CharField(max_length=1000)
+
+class Bus(models.Model):
+    bus_number = models.CharField(max_length=20)
+    bus_driver = models.ForeignKey(Person)
+    bus_depot = models.ForeignKey(Depot)
